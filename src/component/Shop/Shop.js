c546e6ea47e3c25c6cbe2849c27ba234a1d61ff6
Nafees143 <menafees143@gmail.com>
2020-08-23 05:32:34 +0600

    colitese

---
+++ src/component/Shop/Shop.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import fakeData from '../../fakeData';
+import './Shop.css';
+import Product from '../Product/Product';
+
+const Shop = () => {
+    const first10 = fakeData.slice(0,10);
+    const [products, setProducts] = useState(first10);
+
+
+    return (
+        <div className = "shop-container">
+            <h1>This is the Shop</h1>
+            <h3>{products.length}</h3>
+            <div className="product-container">
+                
+                    {
+                        products.map(pd => <Product product = {pd}></Product>)
+                    }
+
+            </div>
+            <div className="cart-container">
+                <h3>This is cart</h3>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
